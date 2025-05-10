8a545d6cdd9350bf47b21c570536965aab269587
jcalle19 <jcalle19@asu.edu>
2025-05-09 21:36:39 -0700

    Added expand button at the bottom
    Added button at the bottom that reveals solar system menu and hides hero section

--- helper.js
+++ helper.js
@@ -1,1 +1,19 @@
-//making from scratch
+const buttons = {
+    navCircle : document.getElementById('nav-circle'),
+    heroSection : document.getElementById('hero-section'),
+    upArrow : document.getElementById('up-arrow-button'),
+    setAnimations : function() {
+        this.upArrow.addEventListener('click', () => {
+            this.navCircle.classList.remove('expand-animation');
+            this.heroSection.classList.remove('expand-animation-reverse');
+            void this.navCircle.offsetWidth;
+            void this.heroSection.offsetWidth;
+            this.navCircle.classList.add('expand-animation');
+            this.heroSection.classList.add('expand-animation-reverse');
+        });
+    },
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    buttons.setAnimations();
+});
