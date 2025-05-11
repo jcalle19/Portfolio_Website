d917013c98f495a61bc478ce4dd266b2ce1d65a4
jcalle19 <jcalle19@asu.edu>
2025-05-10 22:54:46 -0700

    added down arrow button
    added down arrow button that brings back main menu

--- helper.js
+++ helper.js
@@ -2,14 +2,27 @@ const buttons = {
     navCircle : document.getElementById('nav-circle'),
     heroSection : document.getElementById('hero-section'),
     upArrow : document.getElementById('up-arrow-button'),
+    downArrow: document.getElementById('down-arrow-button'),
     setAnimations : function() {
         this.upArrow.addEventListener('click', () => {
             this.navCircle.classList.remove('expand-animation');
+            this.navCircle.classList.remove('expand-animation-reverse');
             this.heroSection.classList.remove('expand-animation-reverse');
+            this.heroSection.classList.remove('expand-animation');
             void this.navCircle.offsetWidth;
             void this.heroSection.offsetWidth;
             this.navCircle.classList.add('expand-animation');
             this.heroSection.classList.add('expand-animation-reverse');
+        });
+        this.downArrow.addEventListener('click', () => {
+            this.navCircle.classList.remove('expand-animation');
+            this.navCircle.classList.remove('expand-animation-reverse');
+            this.heroSection.classList.remove('expand-animation-reverse');
+            this.heroSection.classList.remove('expand-animation');
+            void this.navCircle.offsetWidth;
+            void this.heroSection.offsetWidth;
+            this.navCircle.classList.add('expand-animation-reverse');
+            this.heroSection.classList.add('expand-animation');
         });
     },
 }
